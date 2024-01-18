c6feee11ca0810568aead12cde96081b7891299c
Patrick-Borj-Tuano <154597846+Patrick-Borj-Tuano@users.noreply.github.com>
2024-01-18 17:58:41 +0800

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game5
+{
+    internal class Projectile
+    {
+        public Bullet bullet;
+
+        public Projectile() { bullet = new Bullet(); }
+
+        public class Bullet
+        {
+            public PictureBox PictureBox { get; set; }
+            public int Damage { get; set; }
+
+            public Bullet()
+            {
+           
+            }
+
+            public Bullet(PictureBox pictureBox, int damage)
+            {
+                PictureBox = pictureBox;
+                Damage = damage;
+            }
+        }
+    }
+}
